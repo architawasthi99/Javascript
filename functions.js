d733f1647f3755a74f3cae31f26be21ac8c6009a
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-06-10 10:58:02 +0530

    functions , arrow functions, foreach loop

--- functions.js
+++ functions.js
@@ -4,7 +4,30 @@ const multi=(a,b)=>{
     console.log(a*b);
 }
 multi(5,3);
+ function myFunction(){
+    console.log("hello world");
+ }
+ myFunction();
 
+ function hello(msg){
+    console.log(msg);
+ }
+ hello("welcome to javascript"); 
+
+
+ //ARROW FUNCTIONS
+const mul =(a,b)=>a*b;{
+   console.log(mul(5,5));
+}
+
+const multi=(a,b)=>{
+   console.log(a*b);
+}
+multi(5,5);
+
+const add=(a,b)=>{
+   console.log(a+b);
+}
 //foreach loop
 let arr=[1,2,3,4,5,6,7,8]
 arr.forEach(function printval(val){
