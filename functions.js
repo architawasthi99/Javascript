ac6994a9fc4b9123adf90f22efcc2f8c3a19282e
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-06-11 12:26:16 +0530

    Refactor functions

--- functions.js
+++ functions.js
@@ -1,9 +1,3 @@
-//ARROW FUNCTIONS
-//multiplication
-const multi=(a,b)=>{
-    console.log(a*b);
-}
-multi(5,3);
  function myFunction(){
     console.log("hello world");
  }
@@ -14,6 +8,26 @@ multi(5,3);
  }
  hello("welcome to javascript"); 
 
+//TAKES STRINGS AND RETURN VOWELS
+function vowel(str){
+   let count=0;
+   for(let str1 of str){
+      if("aeiouAEIOU".includes(str1)){
+         count++;
+      }
+   }
+   console.log(count);
+}vowel("architawasthi");
+
+const vowelcount=(str)=>{
+   let count=0;
+   for(let str1 of str){
+      if("aeiouAEIOU".includes(str1)){
+         count++;
+      }
+   }
+   console.log(count);
+};vowelcount("architawasthi");
 
  //ARROW FUNCTIONS
 const mul =(a,b)=>a*b;{
@@ -28,15 +42,37 @@ multi(5,5);
 const add=(a,b)=>{
    console.log(a+b);
 }
-//foreach loop
-let arr=[1,2,3,4,5,6,7,8]
+
+//FOR - EACH LOOP
+let arr=[1,2,3,4,5,6,7];
 arr.forEach(function printval(val){
-    console.log(val)
-})
+   console.log(val);
+});
 
-//find square of each number
+let brr=[11,22,33,44,55,66];
+brr.forEach((val) =>{
+   console.log(val);
+});
 
-let brr=[1,2,3,4,5,6]
-brr.forEach((val)=>{
-    console.log(val*val);
-})
+let crr=[1,2,3,4,5,6,7,8];
+crr.forEach((val)=>{
+   console.log(val*val);
+});
+
+//PRINT ARRAY USING MAP FUNCTION
+let drr=[1,2,3,4];
+drr.map((val)=>{
+   console.log(val);
+});
+//PRINT NEW ARRAY USING MAP FUNCTION
+let err=[67,68,69];
+let newerr=err.map((val)=>{
+   return val;
+});
+console.log(newerr);
+//PRINT SQUARE OF ARRAY USING MAP FUNCTION
+let frr=[77,88,99,100];
+let newfrr=frr.map((val)=>{
+   return val*val;
+});
+console.log(newfrr);
