d88fd73c08a5edd2ac71773954a024d8a8950ee2
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-06-27 11:50:16 +0530

    Add files via upload

--- q1.js
+++ q1.js
@@ -1,17 +1,18 @@
-let n=prompt("enter the value of n");
-let arr=[];
-for(let i=1;i<=n;i++){
-    arr.push(i);
+let data="scret information";
+ 
+class user{
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
+    }
+    viewdata(){
+        console.log("website data=",data);
+    }
 }
-console.log(arr);
 
-//addition of all 
-let brr=arr.reduce((prev,curr)=>{
-    return prev+curr;
-});
-console.log(`The sum of ${n} number is `,brr);
-
-//product
-let brr1=arr.reduce((prev,curr)=>{
-    
-});
+let student1=new user("aradhya","aradhya6677@gmail.com");
+let student2=new user("archit","archit2786@gmail.com");
+console.log(student1);
+console.log(student1.viewdata());
+console.log(student2);
+console.log(student2.viewdata());
